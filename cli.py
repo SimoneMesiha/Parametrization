9881979a389e76006f198c5fee3ed4b416c901d0
SimoneMesiha <53223014+SimoneMesiha@users.noreply.github.com>
2024-06-02 21:24:55 -0400

    added CLI solution so that anyone who downloads the package and can run the code on a terminal

---
+++ cli.py
@@ -0,0 +1,19 @@
+import argparse
+from parametrization.parametrization import calculate_volume
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate the volume of a sphere where R is the radius')
+    parser.add_argument('radius', type=float, help='Radius of the sphere')
+    args = parser.parse_args()
+
+    try:
+        volume = calculate_volume(args.radius)
+        print(f'The volume of the sphere is: {volume}')
+    except ValueError as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
